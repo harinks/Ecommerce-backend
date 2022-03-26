2d1f9ce13fb9b46fa1b2bc6bbc943d086ccabbec
nkshariharan <nkshariharan17@gmail.com>
2022-03-26 23:07:31 +0530

    ordermodel and order route

---
+++ Routes/orderRoutes.js
@@ -0,0 +1,98 @@
+const orderRouter = require("express").Router();
+const asyncHandler = require("express-async-handler");
+const protect = require("../Middleware/AuthMiddleware");
+const Order = require("./../Models/OrderModel");
+
+// CREATE ORDER
+orderRouter.post(
+  "/",
+  protect,
+  asyncHandler(async (req, res) => {
+    const {
+      orderItems,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    } = req.body;
+
+    if (orderItems && orderItems.length === 0) {
+      res.status(400);
+      throw new Error("No order items");
+      return;
+    } else {
+      const order = new Order({
+        orderItems,
+        user: req.user._id,
+        shippingAddress,
+        paymentMethod,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice,
+      });
+
+      const createOrder = await order.save();
+      res.status(201).json(createOrder);
+    }
+  })
+);
+
+// USER LOGIN ORDERS
+orderRouter.get(
+  "/",
+  protect,
+  asyncHandler(async (req, res) => {
+    const order = await Order.find({ user: req.user._id }).sort({ _id: -1 });
+    res.json(order);
+  })
+);
+
+// GET ORDER BY ID
+orderRouter.get(
+  "/:id",
+  protect,
+  asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id).populate(
+      "user",
+      "name email"
+    );
+
+    if (order) {
+      res.json(order);
+    } else {
+      res.status(404);
+      throw new Error("Order Not Found");
+    }
+  })
+);
+
+// ORDER IS PAID
+orderRouter.put(
+  "/:id/pay",
+  protect,
+  asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+
+    if (order) {
+      order.isPaid = true;
+      order.paidAt = Date.now();
+      order.paymentResult = {
+        id: req.body.id,
+        status: req.body.status,
+        update_time: req.body.update_time,
+        email_address: req.body.email_address,
+      };
+
+      const updatedOrder = await order.save();
+      res.json(updatedOrder);
+    } else {
+      res.status(404);
+      throw new Error("Order Not Found");
+    }
+  })
+);
+
+module.exports = orderRouter;
